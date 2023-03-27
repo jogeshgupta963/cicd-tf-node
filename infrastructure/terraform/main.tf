d3c326f74adcf338bc8feab0ca186ff407cdd75d
jogeshgupta963 <jogeshgupta963@gmail.com>
2023-03-27 22:48:20 +0530

    fix:cicd

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -65,6 +65,6 @@ resource "aws_key_pair" "deployer" {
     public_key = var.public_key
 }
 
-output "insance_public_ip"{
+output "instance_public_ip"{
     value = aws_instance.deployer.public_ip
 }
